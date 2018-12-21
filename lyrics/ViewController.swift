99c139a52dc1a3ab0b7911bf8bbc49adc930c24f
yolanda0213 <yolanda0213h@gmail.com>
2018-12-22 01:12:24 +0800

    Initial Commit

---
+++ lyrics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lyrics
+//
+//  Created by Yolanda H. on 2018/12/22.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
